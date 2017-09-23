a17dd7fa0e08e3ec16b59f3de4c629d1101b68a0
Vaibhav Vishwakarma <vaibhiiitm@gmail.com>
2017-09-24 00:16:25 +0530

    ParallelDots API

--- src/paralleldots/ParallelDots.java
+++ src/paralleldots/ParallelDots.java
@@ -32,27 +32,7 @@ public class ParallelDots{
 	}
 
 	public static void main(String[] args) throws Exception {
-		ParallelDots pd = new ParallelDots("wQYG4oIQrkBQsavDfzUIsIKEe4w4hIZaJy9BGlRjpMw");
-		ParallelDots pd = new ParallelDots("pqo9mvej6CkAgNTWGhs0OgkhaedPLsgRBOMXsYNJWsQ");
-		String sentiment = pd.sentiment("Come on, lets play together");
-		System.out.println(sentiment);
-		String similarity = pd.similarity("Sachin is the greatest batsman", "Tendulkar is the finest cricketer");
-		System.out.println(similarity);
-		String ner = pd.ner("Narendra Modi is the prime minister of India");
-		System.out.println(ner);
-		String keywords = pd.keywords("Prime Minister Narendra Modi tweeted a link to the speech Human Resource Development Minister Smriti Irani made in the Lok Sabha during the debate on the ongoing JNU row and the suicide of Dalit scholar Rohith Vemula at the Hyderabad Central University.");
-		System.out.println(keywords);
-		String taxonomy = pd.taxonomy("Narendra Modi is the prime minister of India");
-		System.out.println(taxonomy);
-		String emotion = pd.emotion("Did you hear the latest Porcupine Tree song ? It's rocking !");
-		System.out.println(emotion);
-		String intent = pd.intent("Finance ministry calls banks to discuss new facility to drain cash");
-		System.out.println(intent);
-		String multilang = pd.multilang("La%20ville%20de%20Paris%20est%20tr%C3%A8s%20belle", "fr");
-		System.out.println(multilang);
-		String abuse = pd.abuse("you f**king a$$hole");
-		System.out.println(abuse);
-		
+		System.out.println("ParallelDots API");
 	}
 	public String sentiment(String text) throws Exception {
 		String url = "https://apis.paralleldots.com/v2/sentiment";
